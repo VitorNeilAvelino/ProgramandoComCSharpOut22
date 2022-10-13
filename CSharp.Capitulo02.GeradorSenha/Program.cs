ef0a4680bff0dae5db96c1dbe9e5fd9ecdf3243e
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-10-13 20:18:26 -0300

    Gerador de senha com for

---
+++ CSharp.Capitulo02.GeradorSenha/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharp.Capitulo02.GeradorSenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a quantidade de dígitos da senha (entre 4 e 10, pares): ");
+            var quantidadeDigitos = Convert.ToInt32(Console.ReadLine());
+
+            //if (quantidadeDigitos < 4 || quantidadeDigitos > 10 || quantidadeDigitos % 2 != 0)
+            if (quantidadeDigitos is < 4 or > 10 || quantidadeDigitos % 2 != 0)
+            {
+                Console.WriteLine($"O valor {quantidadeDigitos} é inválido de acordo com as regras.");
+                Console.ReadKey();
+                return;
+            }
+
+            var senha = "";
+            var randomico = new Random();
+
+            for (int i = 0; i < quantidadeDigitos; i++)
+            {
+                //var algarismo = randomico.Next(1, 61);
+                var algarismo = randomico.Next(10);
+
+                senha += algarismo;
+            }
+
+            Console.WriteLine($"Senha gerada: {senha}");
+        }
+    }
+}
