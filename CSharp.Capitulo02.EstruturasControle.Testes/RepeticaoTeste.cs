99db481e901d7eb849732812edf48e22c87790e8
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-10-14 22:49:57 -0300

    Testes com switch

--- CSharp.Capitulo02.EstruturasControle.Testes/RepeticaoTeste.cs
+++ CSharp.Capitulo02.EstruturasControle.Testes/RepeticaoTeste.cs
@@ -19,5 +19,62 @@ namespace CSharp.Capitulo02.EstruturasControle.Testes
                 Console.WriteLine(new string('-', 50));
             }
         }
+
+        [TestMethod]
+        public void EstruturaForTeste()
+        {
+            var i = 1;
+
+            for (Console.WriteLine("Iniciou"); i <= 3; Console.WriteLine(i))
+            {
+                i++;
+            }
+
+            /*
+             for(1. Inicialização; 2. Condição; 4. Pós-execução)
+            {
+                3. Execução
+            }
+             */
+        }
+
+        [TestMethod]
+        public void ForApenasComCondicaoTeste()
+        {
+            var i = 1;
+
+            for (; i <= 3;)
+            {
+                Console.WriteLine(i++);
+            }
+        }
+
+        [TestMethod]
+        public void ContinueTeste()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i <= 5)
+                {
+                    continue;
+                }
+                
+                Console.WriteLine(i);
+            }
+        }
+
+        [TestMethod]
+        public void BreakTeste()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i > 5)
+                {
+                    break;
+                }
+
+                Console.WriteLine(i);
+            }
+        }
     }
 }
