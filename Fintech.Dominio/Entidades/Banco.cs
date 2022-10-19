517b66f2e73e31ef8508707c730357a21b44636d
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-10-18 22:57:31 -0300

    Modelagem de Fintech

---
+++ Fintech.Dominio/Entidades/Banco.cs
@@ -0,0 +1,8 @@
+﻿namespace Fintech.Dominio.Entidades
+{
+    public class Banco
+    {
+        public int Numero { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ Fintech.Dominio/Entidades/ContaCorrente.cs
@@ -0,0 +1,26 @@
+﻿namespace Fintech.Dominio.Entidades
+{
+    public class ContaCorrente
+    {
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public string DigitoVerificador { get; set; }
+        public decimal Saldo { get; set; }
+        public Agencia Agencia { get; set; }
+        public Cliente Cliente { get; set; }
+
+        public void EfetuarOperacao(decimal valor, TipoOperacao tipoOperacao)
+        {
+            switch (tipoOperacao)
+            {
+                case TipoOperacao.Deposito:
+                    Saldo += valor;
+                    //Saldo = Saldo + valor;
+                    break;
+                case TipoOperacao.Saque:
+                    Saldo -= valor;
+                    break;
+            }
+        }
+    }
+}
