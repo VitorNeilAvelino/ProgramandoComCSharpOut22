564b5cd20dfd3ea291eb7f9a5572f40cbe76e82d
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-10-25 22:56:54 -0300

    Começo do repositório de Movimento

---
+++ Fintech.Repositorios.SistemaArquivos/MovimentoRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Fintech.Dominio.Entidades;
+using System;
+
+namespace Fintech.Repositorios.SistemaArquivos
+{
+    public class MovimentoRepositorio
+    {
+        public void Inserir(Movimento movimento)
+        {
+
+        }
+    }
+}
