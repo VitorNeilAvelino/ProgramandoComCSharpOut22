7ccd1ba944fc66291338cd6ab06e7d59c06a81c9
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-11-05 16:48:10 -0300

    Delegate Action

--- Fintech.Repositorios.SistemaArquivosTests/MovimentoRepositorioTests.cs
+++ Fintech.Repositorios.SistemaArquivosTests/MovimentoRepositorioTests.cs
@@ -31,5 +31,27 @@ namespace Fintech.Repositorios.SistemaArquivos.Tests
                 Console.WriteLine($"{movimento.Data} - {movimento.Operacao} - {movimento.Valor}");
             }
         }
+
+        [TestMethod]
+        public void DelegateActionTeste()
+        {
+            var movimentos = movimentoRepositorio.Selecionar(123, 456);
+
+            //foreach (var movimento in movimentos)
+            //{
+            //    Console.WriteLine($"{movimento.Data} - {movimento.Operacao} - {movimento.Valor:c}");
+            //}
+
+            Action<Movimento> writeLine = m => Console.WriteLine($"{m.Data} - {m.Operacao} - {m.Valor:c}");
+
+            //movimentos.ForEach(EscreverMovimento);
+            //movimentos.ForEach(writeLine);
+            movimentos.ForEach(m => Console.WriteLine($"{m.Data} - {m.Operacao} - {m.Valor:c}"));
+        }
+
+        private void EscreverMovimento(Movimento movimento)
+        {
+            Console.WriteLine($"{movimento.Data} - {movimento.Operacao} - {movimento.Valor:c}");
+        }
     }
 }
